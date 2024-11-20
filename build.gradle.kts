f7d45246cc43b22bd4c6bf63881d72f05ba85e14
parulc7 <parul.chandel14@gmail.com>
2024-11-20 20:14:51 +0530

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+application {
+    mainClass.set("Main")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
---
+++ src/main/kotlin/money/Change.kt
@@ -0,0 +1,42 @@
+import java.util.TreeMap
+
+class Change {
+    private val map by lazy {
+        TreeMap<MonetaryElement, Int>(Comparator { lhs, rhs ->
+            lhs.minorValue.compareTo(rhs.minorValue)
+        })
+    }
+
+    var total: Long = 0
+        private set
+
+    fun getCount(element: MonetaryElement): Int {
+        return map[element] ?: 0
+    }
+
+    fun add(element: MonetaryElement, count: Int): Change {
+        return modify(element, count)
+    }
+
+    fun remove(element: MonetaryElement, count: Int): Change {
+        return modify(element, -count)
+    }
+
+    private fun modify(element: MonetaryElement, count: Int): Change {
+        val newCount = (map[element] ?: 0) + count
+        require (newCount >= 0) {"Resulting count is less than zero." }
+        if (newCount == 0) {
+            map.remove(element)
+        } else {
+            map[element] = newCount
+        }
+        total += element.minorValue/100 * count
+        return this
+    }
+
+    companion object {
+
+        fun none(): Change =
+            Change()
+    }
+}
